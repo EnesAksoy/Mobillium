f85d3ee81c6e33cb5b6cdfc94b3634edc79061da
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-13 11:39:53 +0300

    Added response models.

---
+++ Mobillium-EnesAksoy/Common/Models/ResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseModel.swift
+//  Mobillium-EnesAksoy
+//
+//  Created by ENES AKSOY on 13.02.2021.
+//  Copyright © 2021 ENES AKSOY. All rights reserved.
+//
+
+import Foundation
+
+struct ResponseModel: Codable {
+    let results: [ResultModel]
+}
---
+++ Mobillium-EnesAksoy/Common/Models/ResultModel.swift
@@ -0,0 +1,15 @@
+//
+//  ResultModel.swift
+//  Mobillium-EnesAksoy
+//
+//  Created by ENES AKSOY on 13.02.2021.
+//  Copyright © 2021 ENES AKSOY. All rights reserved.
+//
+
+import Foundation
+
+struct ResultModel: Codable {
+    let title: String
+    let overview: String
+    let id: Int
+}
