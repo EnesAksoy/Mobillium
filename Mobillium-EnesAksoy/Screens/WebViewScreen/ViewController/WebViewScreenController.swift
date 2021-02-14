78cae6cde5d85e1186fc33a87cf65747498c41a7
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-14 10:35:51 +0300

    Added web view.

---
+++ Mobillium-EnesAksoy/Screens/WebViewScreen/ViewController/WebViewScreenController.swift
@@ -0,0 +1,36 @@
+//
+//  WebViewScreenController.swift
+//  Mobillium-EnesAksoy
+//
+//  Created by ENES AKSOY on 14.02.2021.
+//  Copyright © 2021 ENES AKSOY. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewScreenController: UIViewController {
+
+    //MARK: - Proporties
+    private let imdbBaseUrl = "https://www.imdb.com/title/"
+    
+    //MARK:- Outlets
+    @IBOutlet weak var webview: WKWebView!
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.callWebsite()
+    }
+
+    //MARK:- Functions
+    private func callWebsite() {
+        let imdbUrl = URL(string: "\(imdbBaseUrl)\(ObjectStore.shared.imdbId ?? "")")
+        let request = URLRequest(url: imdbUrl!)
+        webview.load(request)
+    }
+
+   
+
+}
